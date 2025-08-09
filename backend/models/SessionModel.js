d4b56755fe314905232b283a3be1cc8eb94f86f4
sundram <ram322222@gmail.com>
2025-08-09 10:30:33 +0530

    created session model

---
+++ backend/models/SessionModel.js
@@ -0,0 +1,50 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../../config/DatabaseConfig.js';
+
+const SessionModel = sequelize.define(
+    'Session', {
+        sid: {
+            type: DataTypes.STRING(255),
+            primaryKey: true
+        },
+      
+        sessionData: {  
+            type: DataTypes.JSON,
+            allowNull: false,
+            defaultValue: {
+                userId: null,
+                gameId: null,      
+                socketId: null,    
+                lastActivity: null,  
+                userAgent: null      
+            }
+        },
+        expire: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            precision: 6
+        },
+
+        ipAddress: {
+            type: DataTypes.STRING(45), 
+            allowNull: true
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+            allowNull: false
+        }
+    },
+    {
+        indexes: [
+         
+            { fields: ['expire'] },
+         
+            { fields: [sequelize.json('sessionData.userId')] },
+           
+            { fields: [sequelize.json('sessionData.gameId')] }
+        ]
+    }
+);
+
+export { SessionModel };
